c4d52c0c27cd64517f3b655fe111a3826636c144
mukireus <muhammet.omer@outlook.com>
2019-09-03 12:11:22 +0300

    added slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-List<String> imagePath = [
-  "assets/images/book.png"
-      "assets/images/shop.png"
-      "assets/images/box.png"
-];
-
-List<String> title = [
-  "Discounted Secondhand Books"
-      "20 Book Grocers Nationally"
-      "Sell or Recycle Your Old Books With Us"
-];
-
-List<String> description = [
-  "Used and near new secondhand books at great prices."
-      "We've successfully opened 20 stores across Australia."
-      "If you're looking to downsize, sell or recycle old books, the Book Grocer can help."
-];
-
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Application Intro',
       home: HomePage(),
     ));
 
+List<String> imagePath = [
+  "assets/images/book.png",
+  "assets/images/shop.png",
+  "assets/images/box.png"
+];
+
+List<String> title = [
+  "Discounted Secondhand Books",
+  "20 Book Grocers Nationally",
+  "Sell or Recycle Your Old Books With Us"
+];
+
+List<String> description = [
+  "Used and near new secondhand books at great prices.",
+  "We've successfully opened 20 stores across Australia.",
+  "If you're looking to downsize, sell or recycle old books, the Book Grocer can help."
+];
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: ContentPage(),
     );
   }
@@ -45,13 +46,11 @@ class ContentPage extends StatelessWidget {
       viewportFraction: 1.0,
       aspectRatio: MediaQuery.of(context).size.aspectRatio,
       height: MediaQuery.of(context).size.height,
-      items: [0, 1, 2, 3].map((i) {
+      items: [0, 1, 2].map((i) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(color: Colors.white),
-                child: AppIntro(i));
+                width: MediaQuery.of(context).size.width, child: AppIntro(i));
           },
         );
       }).toList(),
@@ -75,14 +74,33 @@ class _AppIntroState extends State<AppIntro> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          child: Column(
-
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(70.0),
+            child: Text(
+              title[widget.index],
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 35, color: Color(0xFF5ABD8C)),
+            ),
           ),
-        )
-      ],
+          Text(
+            description[widget.index],
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 19, color: Color(0xFFAFDFC7)),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                Image.asset(imagePath[widget.index],
+                    width: MediaQuery.of(context).size.width),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
