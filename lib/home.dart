c349c06117b1ad7d4cf5ca1505fd6b2f93ef89f5
mukireus <muhammet.omer@outlook.com>
2019-09-09 01:56:52 +0300

    Update Home Screen and Added Books Assets

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ui_helper.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -19,50 +20,88 @@ class _MainContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      scrollDirection: Axis.vertical,
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0),
-          child: new Container(
-            child: new Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: new Text(
-                        "Our Top Picks",
-                        style: new TextStyle(
-                            fontSize: 30.0, fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: new IconButton(
-                          icon: Icon(Icons.menu),
-                          iconSize: 30,
-                          onPressed: () {},
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
+        AppBar(),
+        Container(
+          height: 240.0,
+          child:
+              ListView(scrollDirection: Axis.horizontal, children: bookCards),
         )
       ],
     );
   }
 }
 
-Widget get _bookCard => Card();
+class AppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: new Container(
+            child: new Column(children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: new Text(
+                  "Our Top Picks",
+                  style: new TextStyle(
+                      fontSize: 30.0, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: new IconButton(
+                    icon: Icon(Icons.menu),
+                    iconSize: 30,
+                    onPressed: () {},
+                  ),
+                ),
+              )
+            ],
+          ),
+        ])));
+  }
+}
+
+class BookCard extends StatelessWidget {
+  final String imagePath, author, bookName;
+
+  BookCard(this.imagePath, this.author, this.bookName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: new BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Color(0x75000000),
+                  blurRadius: 15.0, // has the effect of softening the shadow
+                  offset: Offset(
+                    0, // horizontal, move right
+                    4.0, // vertical, move down
+                  ),
+                ),
+              ]),
+              height: 210,
+              width: 137,
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui_helper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+List<BookCard> bookCards = [
+  BookCard("assets/images/books/book_zola.png", "Michael Rosen",
+      "The Dissapearance of Emila Zola"),
+  BookCard("assets/images/books/book_fatherhood.png", "Marcus Berkmann",
+      "Fatherhood"),
+  BookCard("assets/images/books/book_space.png", "Stride Lottie",
+      "The Time Travellers Handbook"),
+  BookCard("assets/images/books/book_tree.png", "Stride Lottie",
+      "The Time Travellers Handbook"),
+];
