ce3561a8f9a76671f6c10fb11c8037b667c9868a
mukireus <muhammet.omer@outlook.com>
2019-09-09 16:42:50 +0300

    Update Home Screen v1.2

--- lib/home.dart
+++ lib/home.dart
@@ -24,20 +24,55 @@ class _MainContent extends StatelessWidget {
       children: <Widget>[
         AppBar(),
         Container(
-          height: 240.0,
+          height: 350.0,
           child:
-              ListView(scrollDirection: Axis.horizontal, children: bookCards),
-        )
+              ListView(scrollDirection: Axis.horizontal, children: OurTopPicks),
+        ),
+        TextBestSellers,
+        Container(
+          height: 350.0,
+          child: ListView(
+              scrollDirection: Axis.horizontal, children: BestSellersBook),
+        ),
+        TextGenres,
+        TextRecentlyViewed
       ],
     );
   }
 }
 
+Widget get TextBestSellers => Padding(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+      child: Text(
+        "Bestsellers",
+        style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.left,
+      ),
+    );
+
+Widget get TextGenres => Padding(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+      child: Text(
+        "Genres",
+        style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.left,
+      ),
+    );
+
+Widget get TextRecentlyViewed => Padding(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+      child: Text(
+        "Recently Viewed",
+        style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.left,
+      ),
+    );
+
 class AppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
         child: new Container(
             child: new Column(children: <Widget>[
           Row(
@@ -66,41 +101,125 @@ class AppBar extends StatelessWidget {
   }
 }
 
-class BookCard extends StatelessWidget {
+class OurTopPicksCard extends StatelessWidget {
   final String imagePath, author, bookName;
 
-  BookCard(this.imagePath, this.author, this.bookName);
+  OurTopPicksCard(this.imagePath, this.author, this.bookName);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(
-          Radius.circular(10.0),
-        ),
-        child: Stack(
-          children: <Widget>[
-            Container(
-              decoration: new BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Color(0x75000000),
-                  blurRadius: 15.0, // has the effect of softening the shadow
-                  offset: Offset(
-                    0, // horizontal, move right
-                    4.0, // vertical, move down
-                  ),
+      padding: const EdgeInsets.fromLTRB(25, 20, 25, 0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Color(0x50000000),
+                blurRadius: 10.0, // has the effect of softening the shadow
+                offset: Offset(
+                  0, // horizontal, move right
+                  10.0, // vertical, move down
                 ),
-              ]),
-              height: 210,
-              width: 137,
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.cover,
+              ),
+            ]),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    height: 210,
+                    width: 137,
+                    child: Image.asset(
+                      imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              bookName,
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              author,
+              style: TextStyle(fontSize: 13, color: Color(0x50242126)),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellersCard extends StatelessWidget {
+  final String imagePath, author, bookName, stars;
+
+  BestSellersCard(this.imagePath, this.author, this.bookName, this.stars);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(25, 20, 25, 0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Color(0x50000000),
+                blurRadius: 10.0, // has the effect of softening the shadow
+                offset: Offset(
+                  0, // horizontal, move right
+                  10.0, // vertical, move down
+                ),
+              ),
+            ]),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    height: 210,
+                    width: 137,
+                    child: Image.asset(
+                      imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              bookName,
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              author,
+              style: TextStyle(fontSize: 13, color: Color(0x50242126)),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/ui_helper.dart
+++ lib/ui_helper.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-List<BookCard> bookCards = [
-  BookCard("assets/images/books/book_zola.png", "Michael Rosen",
-      "The Dissapearance of Emila Zola"),
-  BookCard("assets/images/books/book_fatherhood.png", "Marcus Berkmann",
+List<OurTopPicksCard> OurTopPicks = [
+  OurTopPicksCard("assets/images/books/book_zola.png", "Michael Rosen",
+      "The \n Dissapearance of \n Emila Zola"),
+  OurTopPicksCard("assets/images/books/book_fatherhood.png", "Marcus Berkmann",
       "Fatherhood"),
-  BookCard("assets/images/books/book_space.png", "Stride Lottie",
-      "The Time Travellers Handbook"),
-  BookCard("assets/images/books/book_tree.png", "Stride Lottie",
-      "The Time Travellers Handbook"),
+  OurTopPicksCard("assets/images/books/book_space.png", "Stride Lottie",
+      "The Time \n Travellers Handbook"),
+  OurTopPicksCard("assets/images/books/book_tree.png", "Stride Lottie",
+      "The Time \n Travellers Handbook"),
+];
+
+List<BestSellersCard> BestSellersBook = [
+  BestSellersCard("assets/images/books/book_zoo.png", "Christopher Wilson",
+      "Zoo", "4"),
+  BestSellersCard("assets/images/books/book_paper.png", "Rebecca Mackenzie",
+      "In A Land Of Paper Gods", "5"),
+  BestSellersCard("assets/images/books/book_tattletale.png", "Sarah J. Noughton",
+      "Tattletale", "4"),
 ];
